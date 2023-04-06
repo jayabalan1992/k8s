afbf569bd541afa7b60891f1cb597f86d062e7fd
jayabalan <jayapalan1992@gmail.com>
2023-04-06 22:05:44 +0000

    new script

---
+++ k8s_components_bootstrap.sh
@@ -0,0 +1,5 @@
+sudo apt install kubectl
+sudo gke-gcloud-auth-plugin --version
+sudo   apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+gke-gcloud-auth-plugin --version
+gcloud container clusters get-credentials jay-democluster --region=us-central1
