350155567086999dcbfad4002584e631a19bc264
Jayabalan Bala <jayabalanbala@gmail.com>
2023-09-15 05:09:05 +0000

    labs

--- webapp/pyapp/main.py
+++ webapp/pyapp/main.py
@@ -6,11 +6,15 @@ app = flask.Flask(__name__)
 username = os.getenv('USERNAME')
 @app.route('/')
 def hello():
-  return f"Hello {username}"
+  return flask.render_template('index.html')
 
 @app.route('/otp')
 def randomnum():
   return str(random.randint(1111, 9999))
+
+@app.route('/details')
+def userdetail():
+  return f"Hello {username}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
