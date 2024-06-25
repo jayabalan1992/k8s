6c642417d0b08747805dec909c406d5192519ba2
root <root@k8smanager.us-central1-a.c.inner-replica-426705-c3.internal>
2024-06-25 23:16:14 +0000

    k8s components bootstrap sh

--- k8s_components_bootstrap.sh
+++ k8s_components_bootstrap.sh
@@ -1,13 +1,24 @@
-sudo apt install kubectl
-sudo gke-gcloud-auth-plugin --version
-sudo   apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-gke-gcloud-auth-plugin --version
-gcloud container clusters get-credentials jay-democluster --region=us-central1
+#!/bin/bash
 
+# Check the operating system flavor and run the appropriate command
 
+if hostnamectl | grep -q "Ubuntu"; then
+        apt install kubectl wget docker -y
+        apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+elif hostnamectl | grep -qE "Rocky|CentOS"; then
+	yum install kubectl wget docker -y
+	yum install google-cloud-sdk-gke-gcloud-auth-plugin -y
+else
+    echo "Unknown operating system flavor"
+    hostnamectl | grep Operating # print the hostnamectl output for debugging purposes
+fi
 
 ## for Centos/Rocky
-
-yum install kubectl -y
 gcloud components install gke-gcloud-auth-plugin
 gcloud container clusters get-credentials jay-democluster --region=us-central1
+
+
+# helm package install
+wget https://get.helm.sh/helm-v3.15.2-linux-amd64.tar.gz
+tar -zxvf helm-v3.15.2-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
