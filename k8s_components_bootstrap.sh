ea9642f50aa0bdb08a57c6a61eec4c9325e4e55f
root <root@k8smanager.us-central1-a.c.inner-replica-426705-c3.internal>
2024-06-25 04:57:09 +0000

    script modify

--- k8s_components_bootstrap.sh
+++ k8s_components_bootstrap.sh
@@ -3,3 +3,11 @@ sudo gke-gcloud-auth-plugin --version
 sudo   apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 gke-gcloud-auth-plugin --version
 gcloud container clusters get-credentials jay-democluster --region=us-central1
+
+
+
+## for Centos/Rocky
+
+yum install kubectl -y
+gcloud components install gke-gcloud-auth-plugin
+gcloud container clusters get-credentials jay-democluster --region=us-central1
